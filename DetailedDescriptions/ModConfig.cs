70af11d4798701ecbd9eba1a0917889a00fed8a4
Admin <Admin@IA>
2024-10-18 00:19:02 -0300

    Em vez de capturar os dados no evento AssetRequest, a logica foi alterada para obter os dados de Game1 no inicio do dia, habilitando a compatibilidade com sementes modificadas.
    Além disso, as descrições agora são aplicadas em Data/Objects em vez de String/Objects.
    
    Implementada lógica inicial para opções de configuração.

--- DetailedDescriptions/ModConfig.cs
+++ DetailedDescriptions/ModConfig.cs
@@ -1,5 +1,26 @@
 ﻿public sealed class ModConfig
 {
-    public int NumberOfCrops { get; set; } = 10;
-    public bool Profit { get; set; } = false;    
+    /// <summary>Exibir a descrição original do item.</summary>
+    public bool ShowOriginalDescription { get; set; } = true;
+
+    /// <summary>Exibir a descrição das estações de plantio.</summary>
+    public bool ShowSeasonDescription { get; set; } = true;
+
+    /// <summary>Exibir a descrição dos dias necessários para colheita.</summary>
+    public bool ShowHarvestInDaysDescription { get; set; } = true;
+
+    /// <summary>Exibir a descrição sobre a necessidade de replantio ou o tempo de regeneração.</summary>
+    public bool ShowRegrowDescription { get; set; } = true;
+
+    /// <summary>Exibir a descrição do preço de compra das sementes.</summary>
+    public bool ShowBuyPriceDescription { get; set; } = true;
+
+    /// <summary>Exibir a descrição do preço de venda da colheita.</summary>
+    public bool ShowSellPriceDescription { get; set; } = true;
+
+    /// <summary>Exibir a descrição do lucro por ciclo.</summary>
+    public bool ShowProfitPerCycleDescription { get; set; } = true;
+
+    /// <summary>Exibir a descrição da rentabilidade (taxa de lucro) por ciclo.</summary>
+    public bool ShowProfitabilityDescription { get; set; } = true;
 }
--- DetailedDescriptions/ModEntry.cs
+++ DetailedDescriptions/ModEntry.cs
@@ -12,42 +12,26 @@ namespace DetailedDescriptions
     {
         private ModConfig Config;
 
+        // Dicionario "Data/Crops"
         private IDictionary<string, CropData> cropsData;
+        // Dicionario "Data/Objects"
         private IDictionary<string, ObjectData> objectsData;
+        // Dicionario "Strings/Objects"
+        private IDictionary<string, string> stringsData;
 
         public override void Entry(IModHelper helper)
         {
             this.Config = this.Helper.ReadConfig<ModConfig>();
 
-            int numberOfCrops = Config.NumberOfCrops;
-            bool Profit = Config.Profit;
-
-            //helper.Events.Content.AssetRequested += OnAssetRequested;
             helper.Events.GameLoop.GameLaunched += OnGameLaunched;
             helper.Events.GameLoop.DayStarted += OnDayStarted;
-
         }
 
         private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
         {
             var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
-            if (configMenu is null)
-                return;
-
-            configMenu.Register(
-                mod: this.ModManifest,
-                reset: () => this.Config = new ModConfig(),
-                save: () => this.Helper.WriteConfig(this.Config)
-            );
-
-            configMenu.AddBoolOption(
-                mod: this.ModManifest,
-                name: () => "Habilitar lucro?",
-                tooltip: () => "Exibir o Lucro?",
-                getValue: () => this.Config.Profit,
-                setValue: value => this.Config.Profit = value
-            );
         }
+
         private void OnDayStarted(object sender, DayStartedEventArgs e)
         {
             this.Helper.GameContent.InvalidateCache("Data/Objects");
@@ -69,16 +53,14 @@ namespace DetailedDescriptions
                 if (objectsData.ContainsKey(cropKey) && objectsData.ContainsKey(harvestKey))
                 {
                     ObjectData seedObject = objectsData[cropKey]; // Dados do item da semente
-                    ObjectData harvestObject = objectsData[harvestKey]; // Dados do item colhido
-                   
-                    string originalDescription = seedObject.Description; // Verifica a descrição original da semente e extrai a chave
+                    ObjectData harvestObject = objectsData[harvestKey]; // Dados do item colhido                   
 
                     // Cálculos para a nova descrição
                     int seed_daysinphase_sum = cropData.DaysInPhase.Sum();
                     int total_cycle_days = cropData.Seasons.Count * 28;
                     int harvests = cropData.RegrowDays == -1 ? total_cycle_days / seed_daysinphase_sum : 1 + ((28 - seed_daysinphase_sum) / cropData.RegrowDays);
                     int seed_buy_value = seedObject.Price * 2;
-                    int profit_cycle = (harvests * harvestObject.Price) - seed_buy_value;                    
+                    int profit_cycle = (harvests * harvestObject.Price) - seed_buy_value;
                     int total_work = cropData.RegrowDays == -1 ? harvests * 2 : harvests; // Cálculo do trabalho total (colheitas e replantios)                    
                     double profitability = total_work > 0 ? (double)profit_cycle / total_work : 0; // Calcular rentabilidade                   
                     List<StardewValley.Season> seasons = cropData.Seasons; // Obtém as estações do cultivo (ex.: "Spring", "Summer", etc.)                                     
@@ -87,37 +69,72 @@ namespace DetailedDescriptions
                         .ToArray(); // Converte a lista para array de strings                   
                     string translatedSeasonsString = string.Join(", ", seasonNames.Select(name => this.Helper.Translation.Get($"season.{name}"))); // Traduz cada nome de estação usando o sistema de tradução do SMAPI
 
-                    // Cria a descrição final com as estações traduzidas
-                    string seasonDescription = $"{this.Helper.Translation.Get("season")}: {translatedSeasonsString}.";
-                    string harvestInDaysDescription = this.Helper.Translation.Get("harvestInDays", new { days = seed_daysinphase_sum });
-                    string regrowDescription = (cropData.RegrowDays == -1
+                    List<string> newDescription = new List<string>();
+
+                    // Textos para a descrição da semente
+                    if (Config.ShowOriginalDescription)
+                    {
+                        newDescription.Add(seedObject.Description);
+                        newDescription.Add("");
+                    }
+
+                    if (Config.ShowSeasonDescription)
+                    {
+                        string seasonDescription = $"{this.Helper.Translation.Get("season")}: {translatedSeasonsString}.";
+                        newDescription.Add(seasonDescription);
+                    }
+
+                    if (Config.ShowHarvestInDaysDescription)
+                    {
+                        string harvestInDaysDescription = this.Helper.Translation.Get("harvestInDays", new { days = seed_daysinphase_sum });
+                        newDescription.Add(harvestInDaysDescription);
+                    }
+
+                    if (Config.ShowRegrowDescription)
+                    {
+                        string regrowDescription = (cropData.RegrowDays == -1
                         ? this.Helper.Translation.Get("needsReplanting")
                         : this.Helper.Translation.Get("regrowInDays", new { days = cropData.RegrowDays }));
-                    string buyPriceDescription = this.Helper.Translation.Get("buyPrice", new { price = seed_buy_value });
-                    string sellPriceDescription = this.Helper.Translation.Get("sellPrice", new { price = harvestObject.Price });
+                        newDescription.Add(regrowDescription);
+                    }
+
+                    if (Config.ShowBuyPriceDescription)
+                    {
+                        string buyPriceDescription = this.Helper.Translation.Get("buyPrice", new { price = seed_buy_value });
+                        newDescription.Add(buyPriceDescription);
+                    }
+
+                    if (Config.ShowSellPriceDescription)
+                    {
+                        string sellPriceDescription = this.Helper.Translation.Get("sellPrice", new { price = harvestObject.Price });
+                        newDescription.Add(sellPriceDescription);
+                    }
 
                     // Informações de lucro por ciclo
-                    string profitPerCycleDescription = this.Helper.Translation.Get("profitPerCycle", new { totalDays = total_cycle_days });
-                    string harvestsDescription = this.Helper.Translation.Get("harvests", new { count = harvests });
-                    string profitDescription = this.Helper.Translation.Get("profit", new { value = profit_cycle });
-                    string profitabilityDescription = this.Helper.Translation.Get("profitability", new { rate = profitability.ToString("F2") });
+                    if (Config.ShowProfitPerCycleDescription)
+                    {
+                        newDescription.Add("");
+                        string profitPerCycleDescription = this.Helper.Translation.Get("profitPerCycle", new { totalDays = total_cycle_days });
+                        string harvestsDescription = this.Helper.Translation.Get("harvests", new { count = harvests });
+                        string profitDescription = this.Helper.Translation.Get("profit", new { value = profit_cycle });
+                        newDescription.Add(profitPerCycleDescription);
+                        newDescription.Add(harvestsDescription);
+                        newDescription.Add(profitDescription);
+                    }
 
-                    // Combinando tudo em uma única string
-                    string newDescription = (
-                        seasonDescription + "\n" +
-                        harvestInDaysDescription + "\n" + 
-                        regrowDescription + "\n" +
-                        buyPriceDescription + "\n" +
-                        sellPriceDescription + "\n\n" +
-                        profitPerCycleDescription + "\n" +
-                        harvestsDescription + "\n" +
-                        profitDescription + "\n\n" +
-                        profitabilityDescription
-                    );
+                    // Rentabilidade
+                    if (Config.ShowProfitabilityDescription)
+                    {
+                        newDescription.Add("");
+                        string profitabilityDescription = this.Helper.Translation.Get("profitability", new { rate = profitability.ToString("F2") });
+                        newDescription.Add(profitabilityDescription);
+                    }
 
-                    seedObject.Description = newDescription;
-
-                    this.Monitor.Log(newDescription);
+                    // Atualiza a descrição da semente                    
+                    seedObject.Description = string.Join(Environment.NewLine, newDescription);
+                    this.Monitor.Log($"{newDescription}");
+                    //ou
+                    this.Monitor.Log($"{seedObject.Description}");
                 }
             }
         }
